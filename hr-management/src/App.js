5733482990fcb077542e45fee6767b8999e0017b
suhanigupta03 <gupta03suhani@gmail.com>
2024-03-02 15:38:13 +0530

    todo error
    Co-authored-by: teju0 <tejus0@users.noreply.github.com>

--- hr-management/src/App.js
+++ hr-management/src/App.js
@@ -7,22 +7,73 @@ import {BrowserRouter as Router, Routes,Route } from "react-router-dom";
 import EmployeeAttend from './screens/EmployeeAttend.jsx';
 import EmployeeDetails from './screens/EmployeeDetails.jsx';
 import Calendar from './screens/Calendar.jsx';
+import Todoform from './screens/Todoform.jsx';
+import Todolist from './screens/TodoList.jsx';
 import Modal from 'react-modal';
+import React,{useState} from 'react';
+import {v4} from 'uuid';
 Modal.setAppElement('#root')
 function App() {
+  const[todos,setTodos]=useState([
+    {
+      id:1,
+      title:"Play",
+      isCompleted: false
+    },
+    {
+      id:2,
+      title:"Play Cricket",
+      isCompleted: true
+    },
+    {
+      id:3,
+      title:"Play Cricket",
+      isCompleted: true
+    },
+    {
+      id:4,
+      title:"Play Cricket",
+      isCompleted: true
+    },
+  ]);
+  const checkTodo=(id)=>{
+    console.log(id);
+    setTodos(todos.map(todo => {
+      if(todo.id==id)
+      todo.isCompleted =!todo.isCompleted
+    console.log(todo.isCompleted);
+    return todo;
+    }));
+  };
+  const deleteTodo=(id)=>
+  {
+    setTodos(todos.filter(todo=>todo.id!=id))
+  };
+  const addTodo = (text) =>{
+       const newTodo ={
+         id:v4(),
+         title: text,
+         isCompleted: false,
+       };
+      setTodos([...todos, newTodo]);
+  };
  return (
-      <Router>
-          <Routes>
-            <Route  exact path='/' element={<LandingLayout/>}/>
-            <Route exact path="/payroll" element={<PayrollAdmin />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/leave-management" element={<EmployeeLeave />} />
-            <Route exact path="/attendance-management" element={<EmployeeAttend />} />
-            <Route exact path="/employee-details" element={<EmployeeDetails />} />
-            <Route exact path="/calendar" element={<Calendar />} />
+      // <Router>
+      //     <Routes>
+      //       <Route  exact path='/' element={<LandingLayout/>}/>
+      //       <Route exact path="/payroll" element={<PayrollAdmin />} />
+      //       <Route exact path="/login" element={<Login />} />
+      //       <Route exact path="/leave-management" element={<EmployeeLeave />} />
+      //       <Route exact path="/attendance-management" element={<EmployeeAttend />} />
+      //       <Route exact path="/employee-details" element={<EmployeeDetails />} />
+      //       <Route exact path="/calendar" element={<Calendar />} />
           
-          </Routes>
-      </Router>
+      //     </Routes>
+      // </Router>
+      <div>
+      <Todoform  addTodo={addTodo} />
+      <Todolist todos={todos} checkTodo={checkTodo} deleteTodo={deleteTodo}/>
+      </div>
   );
   
 }
---
+++ hr-management/src/screens/TodoList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Todo from "./Todo";
+
+const todolist=({todos,checkTodo, deleteTodo})=>{
+    return(
+        <div>
+            {
+            todos.map((todo) =>
+            (
+            <Todo key={todo.id}  title={todo.title} checkTodo={checkTodo} id={todo.id} isCompleted={todo.isCompleted} deleteTodo={deleteTodo}/>
+            ))
+            }
+        <Todo />
+        </div>
+    );
+};
+export default todolist;
